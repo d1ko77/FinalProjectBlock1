8fef67c5e4806d73a912b3c9655b75c3965b0fda
Andrew <tropterop@yandex.ru>
2022-11-23 20:40:02 +0300

    Этап 3: запись кода

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -7,3 +7,37 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+string[] NewArray(string[] s, int n)
+{
+    string[] res = new string[CountIndex(s, n)];
+
+    for (int i = 0, j = 0; i < s.Length; i++)
+    {
+        if (s[i].Length <= 3)
+        {
+            res[j] = s[i];
+            j++;
+        }
+    }
+    return res;
+}
+
+int CountIndex(string[] s, int n)
+{
+    int count = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите массив из строк через пробел: ");
+string[] str = Console.ReadLine()!.Split(" ");
+int numOfChar = 3;
+string[] array = NewArray(str, numOfChar);
+Console.WriteLine($"[{String.Join(", ", str)}] -> [{String.Join(", ", array)}]");
